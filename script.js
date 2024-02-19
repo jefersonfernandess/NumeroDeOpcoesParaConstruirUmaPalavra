99ab6c6be088030141adf49e0c7a7567486b05eb
Jeferson Fernandes <dev.jefersonl@gmail.com>
2024-02-19 14:21:29 -0300

    finalização

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 let contadorDasPalavrasConstruidas = 0
 let palavraAlvo = "jeferson";
 let palavraAlvoEmArray = palavraAlvo.split("");
-console.log(palavraAlvoEmArray);
 
 let arrayDePalavras = ["je", "jefe", "rson", "rse", "jeferson"];
 
 function iniciarVerificacao() {
+    let contador = 0;
+    for (let i = 0; i < arrayDePalavras.length; i++) {
+        let palavraAtualDoLoop = arrayDePalavras[i];
 
-    fazerVerificacaoDasPalavras();
+        for (let j = 0; j < arrayDePalavras.length; j++) {
+            let palavraDaVez = juntarDuasPalavras(arrayDePalavras[i], arrayDePalavras[j]);
+            if (palavraAlvo == palavraDaVez) {
+                contadorDasPalavrasConstruidas += 1;
+                console.log("============================================================");
+                console.log(`A palavra ${palavraDaVez} é igual a palavra ${palavraAlvo}`);
+                console.log(palavraDaVez.length);
+                console.log(palavraAtualDoLoop.length);
+                console.log("============================================================");
+            }
+            else {
+                console.log("============================================================");
+                console.log(`A palavra ${palavraDaVez} não é igual a palavra ${palavraAlvo}`);
+                console.log(palavraDaVez.length);
+                console.log(palavraAtualDoLoop.length);
+                console.log("============================================================");
 
-    for(let i = 0; i < arrayDePalavras.length; i++) {
-        let palavraAtualFor = arrayDePalavras[i];
-        let palavraAtualForEmArray = palavraAtualFor.split("");
-        console.log(palavraAtualForEmArray);
-        // let verificandoSeEhAPalavra = palavraAtual + arrayDePalavras[i]
-        // if(verifcarSeEhAPalavra === palavraAlvo) {
-        //     contador += 1
-        // }
-        // console.log(posicaoArrayDePalavras);
+            }
+            contador += 1
+        }
+
     }
+    console.log(`TOTAL: ${contadorDasPalavrasConstruidas}`);
+    console.log(`TOTAL DE TENTATIVAS: ${contador}`);
 }
 
-function fazerVerificacaoDasPalavras() {
-    for(let i = 0; i < arrayDePalavras.length; i++) {
-        if(palavraAlvo == arrayDePalavras[i]) {
-            console.log(`A palavra na posição ${i} é igual a palavra: ${palavraAlvo}`);
-            contadorDasPalavrasConstruidas += 1;
-        }
-        else {
-            console.log(`A palavra na posição ${i} não é igual a palavra: ${palavraAlvo}`);
-        }
-    }
+function juntarDuasPalavras(palavraUm, palavraDois) {
+    return palavraUm + palavraDois
 }
 
-//iniciarVerificacao();
+iniciarVerificacao();
